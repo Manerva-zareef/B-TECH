98ad6ae0f17414bbcccbf6fe62a5b6e4683cf253
Manerva <Manerva Zareef@gmail.com>
2022-09-11 13:50:28 +0200

    adding cart page

---
+++ data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "LG 65 Inch 4K UHD Smart LED TV with Built-in Receiver",
+    price: 12999,
+    desc: "save 2500LE",
+    img: "https://media.btech.com/media/catalog/product/d/v/dv3510nt-cnb-05000ff090-bk-2_7trxggxjjoe1hzxn.jpg?imgeng=w_213,220",
+  },
+  {
+    id: "DFJSDFDS",
+    name: "Ultra Split Air Conditioner, 1.5 HP, 64GB, 4GB RAM,",
+    price: 8649,
+    desc: "save 3690LE",
+    img: "https://media.btech.com/media/catalog/product/d/d/dd5181c3531fe9f04ad7650b61f1d10be96cd32eb8bda62cadfd5c5c94e31a2d.jpeg?imgeng=w_213,220",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Dell Vostro 3510 Laptop, Intel Core i3-1115G4vhhgfhg",
+    price: 9399,
+    desc: "save 2500LE",
+    img: "https://media.btech.com/media/catalog/product/d/d/dd5181c3531fe9f04ad7650b61f1d10be96cd32eb8bda62cadfd5c5c94e31a2d.jpeg?imgeng=w_213,220",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Zanussi Top Load Automatic Washing Machine,",
+    price: 6900,
+    desc: "save 1590LE",
+    img: "https://media.btech.com/media/catalog/product/t/1/t12kg.jpg?imgeng=w_213,220",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Oppo A16K Dual Sim, 64GB, 4GB RAM,",
+    price: 4999,
+    desc: "save 190LE.",
+    img: "https://media.btech.com/media/catalog/product/o/p/oppo-a16k-64gb-4gb-blue_1.jpg?imgeng=w_213,220",
+  },
+];
